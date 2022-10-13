64b9bea8360c3f36f0d4b9356ad3957079e9eb9a
[Davide] <davidpuziello@gmail.com>
2022-10-13 13:46:44 +0200

    Feat: Exercise Update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-class Hello extends React.Component {
-    render() {
-        return <h1>Hello, World!</h1>
-    }
-}
-
-class App extends React.Component {
-    render() {
-        return <div>
-            <Hello />
-        </div>
-    }
-}
+import { App } from './App'
 
 let root = ReactDOM.createRoot(document.getElementById("root"))
 let app = <App />
