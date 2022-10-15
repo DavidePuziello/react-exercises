7195714b50ab2ac21218d100420031a538487eab
[Davide] <davidpuziello@gmail.com>
2022-10-15 12:10:21 +0200

    Feat: Exercise Update

--- src/Age.js
+++ src/Age.js
@@ -3,9 +3,12 @@ import React from 'react'
 export class Age extends React.Component {
         render() {
             return <>
-            {this.props.age > 18 && this.props.age < 65
+            {this.props.age > 18 && this.props.age < 65 /* && this.props.name === "John" */
             ? <p>Your age is {this.props.age}</p>
             : <p>How old are you?</p>}
             </>
         }
 }
+
+//L'esercizio chiede che il "prop.name" di Age deve essere uguale a "Jhon", per questo ho inserito questo commento.
+// Modifica effettuata nel componente "Welcome"
--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
     render() {
         return <>
             <p>Welcome, {this.props.name}</p>
-            <Age age={65}/>
+            {this.props.name === "John" && <Age age={19} />}
         </>
     }
 }
