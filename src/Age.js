b910beb9985fc70c8f73a7583cf3355c3902a363
[Davide] <davidpuziello@gmail.com>
2022-10-15 11:55:49 +0200

    Feat: Exercise Update

--- src/Age.js
+++ src/Age.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export class Age extends React.Component {
-    render() {
-        return <>
-            {!!this.props.age && <p>Your age is {this.props.age}</p>}
-        </>
-    }
+        render() {
+            return <>
+            {this.props.age > 18 && this.props.age < 65
+            ? <p>Your age is {this.props.age}</p>
+            : <p>How old are you?</p>}
+            </>
+        }
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
     render() {
         return <>
             <p>Welcome, {this.props.name}</p>
-            <Age />
+            <Age age={65}/>
         </>
     }
 }
