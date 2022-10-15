6534623feadf0b35ab476002ef341e35d4d1c6b3
[Davide] <davidpuziello@gmail.com>
2022-10-15 12:26:04 +0200

    Feat: Exercise Update

--- src/Age.js
+++ src/Age.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export class Age extends React.Component {
     render() {
-        return <>
-            {!!this.props.age && <p>Your age is {this.props.age}</p>}
-        </>
+        return <p>Your age is {this.props.age}</p>
     }
 }
