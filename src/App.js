1cc9f9492ef2a61037e68bf01423b2046a5edfaf
[Davide] <davidpuziello@gmail.com>
2022-10-22 12:19:55 +0200

    Feat: Exercise Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Hello } from "./Hello";
-import { Welcome } from "./Welcome";
 import { Counter } from "./Counter"
 import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
+import { InteractiveWelcome } from "./InteractiveWelcome";
 
 export class App extends React.Component {
     render() {
         return <div>
             <Hello />
-            <Welcome />
+            <InteractiveWelcome />
             <Counter initialValue={22} incrementAmount={2} incrementInterval={3000}/>
             <ClickCounter />
             <ClickTracker />
