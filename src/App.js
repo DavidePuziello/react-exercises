93301f0d23e96a128bf4c762dbd945465d564fcf
[Davide] <davidpuziello@gmail.com>
2022-10-28 00:05:10 +0200

    Feat: Exercise Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Container } from "./Container";
 import { Hello } from "./Hello";
 import { Login } from "./Login";
 
 export class App extends React.Component {
     render() {
-        return <div>
+        return (
+        <Container>
             <Hello />
             <Login />
-        </div>
+        </Container>
+        )
     }
 }
