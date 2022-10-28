71185fdfabbf7c34593207e94835864cfb822148
[Davide] <davidpuziello@gmail.com>
2022-10-28 19:33:25 +0200

    Feat: Exercise Update

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,3 +1,22 @@
-export function DisplayLanguage() {
+import { CreateContext } from "./LanguageContext"
 
+const Strings = {
+    en: {
+        HELLO : "Hello, this is the English verision"
+    },
+    it: {
+        HELLO : "Ciao, questa è la versione italiana"
+    }
+}
+
+export function DisplayLanguage(){
+    return (
+        <CreateContext.Consumer>
+            {language =>{
+                return (
+                    <h1>{Strings[language].HELLO}</h1>
+                )
+            }}
+        </CreateContext.Consumer>
+    )
 }
