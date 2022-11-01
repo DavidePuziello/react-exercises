3e1abd0218cdc84397715471956a1b40abfcd1c3
[Davide] <davidpuziello@gmail.com>
2022-11-02 00:13:49 +0100

    Feat: Exercise Update

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export function App() {
             <Route path="/" element={<Welcome />} />
             <Route path="/counter" element={<Counter />} />
             <Route path="/users" element={<GithubUserList />}>
+                <Route index element={<h3>Add a user and select it</h3>} />
                 <Route path=":username" element={<ShowGithubUser />} />
             </Route>
             <Route path="*" element={<div><h1>Page not found</h1></div>} />
