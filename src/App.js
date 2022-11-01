70bf4c148788e03d3b53c752726a570dcf3edeed
[Davide] <davidpuziello@gmail.com>
2022-11-01 23:23:44 +0100

    Feat: Exercise Update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome"
 import { Counter } from "./Counter"
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
     return <div>
         <Routes>
-            <Route path="/" element={<Welcome />} />
+            <Route path="/" element={<Welcome name="Stefania" />} />
             <Route path="/counter" element={<Counter />} />
+            <Route path=":username" element={<ShowGithubUser />}>
+            </Route>
         </Routes>
     </div>
 }
