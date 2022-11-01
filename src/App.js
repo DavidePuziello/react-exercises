b5303510cd0cd399d1f4937d5670cc1996803820
[Davide] <davidpuziello@gmail.com>
2022-11-01 23:11:37 +0100

    Feat: Exercise Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome"
+import { Counter } from "./Counter"
 
 export function App() {
     return <div>
         <Routes>
             <Route path="/" element={<Welcome />} />
+            <Route path="/counter" element={<Counter />} />
         </Routes>
     </div>
 }
