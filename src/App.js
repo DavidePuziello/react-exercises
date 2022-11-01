d1a186697f677539a8e402707e1997964f75cb8e
[Davide] <davidpuziello@gmail.com>
2022-11-01 23:29:54 +0100

    Feat: Exercise Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { ShowGithubUser } from "./ShowGithubUser";
 export function App() {
     return <div>
         <Routes>
-            <Route path="/" element={<Welcome name="Stefania" />} />
+            <Route path="/" element={<Welcome />} />
             <Route path="/counter" element={<Counter />} />
             <Route path=":username" element={<ShowGithubUser />}>
             </Route>
